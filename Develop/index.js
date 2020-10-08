ea612f21fc87ccccf56685a0a65a0bd938a0212a
Diana Shull <dianalynshull@gmail.com>
2020-10-08 14:30:18 -0600

    pseudo coded out questions and where they need to be written.

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,41 @@
 // array of questions for user
 const questions = [
-
+    
+    // Title of your application
+    // Write a short description of your application
+    // Enter installation instructions for your application
+    // Application Usage:
+    // Contributors:
+    // Application Tests:
+       // then
+    // Select a license for your application: (select an option)
+    // Github Username:
+    // Email:
+    // Email Instructions for Questions for Application:
+    // Create a table of contents that you can click on to reference any of the above sections
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    // Title
+    // Table of Contents
+    // Badge for License
+    // Description
+    // Installation Instructions
+    // Usage
+    // Contributing
+    // Tests
+    // License
+        // License Info
+    // Questions
+        // Github Username (link to their profile)
+        // Email
+        // Instructions for contact 
 }
 
 // function to initialize program
 function init() {
-
+    // node index.js? Or start?
 }
 
 // function call to initialize program
